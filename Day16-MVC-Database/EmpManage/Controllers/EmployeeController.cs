897d6a4bdd2e2b7ea9bad7d149792f1d3d5c71b0
shantanu02 <shantanuupase02@gmail.com>
2020-12-17 19:13:31 -0600

    17/12/2020 commit

---
+++ Day16-MVC-Database/EmpManage/Controllers/EmployeeController.cs
@@ -0,0 +1,203 @@
+﻿using EmpManage.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmpManage.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private SqlConnection cn = new SqlConnection();
+        
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> emplist = new List<Employee>();
+
+            cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=J&KDec20;Integrated Security=True;Pooling=False";
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from Employees", cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                emplist.Add(new Employee { EmpNo = (int)dr["EmpNo"] , Name = (string)dr["Name"] , Basic = (decimal)dr["Basic"] , DeptNo= (int)dr["DeptNo"] });
+            }
+            cn.Close();
+
+            return View(emplist);
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id = 0)
+        {
+           
+            cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=J&KDec20;Integrated Security=True;Pooling=False";
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from Employees where EmpNo = "+id, cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            Employee emp = new Employee { EmpNo = (int)dr["EmpNo"], Name = (string)dr["Name"], Basic = (decimal)dr["Basic"], DeptNo = (int)dr["DeptNo"] };
+           
+            cn.Close();
+            return View(emp);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            Employee emp = new Employee();
+            return View(emp);
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            try
+            {
+                cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=J&KDec20;Integrated Security=True;Pooling=False";
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "InsertEmployee";
+
+                cmd.Parameters.AddWithValue("@EmpNo", emp.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Basic", emp.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", emp.DeptNo);
+
+                cmd.ExecuteNonQuery();
+
+               
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id=0)
+        {
+            cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=J&KDec20;Integrated Security=True;Pooling=False";
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from Employees where EmpNo = " + id, cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            Employee emp = new Employee { EmpNo = (int)dr["EmpNo"], Name = (string)dr["Name"], Basic = (decimal)dr["Basic"], DeptNo = (int)dr["DeptNo"] };
+
+            cn.Close();
+
+            return View(emp);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id , Employee emp)
+        {
+            try
+            {
+                cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=J&KDec20;Integrated Security=True;Pooling=False";
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "UpdateEmployee";
+
+                cmd.Parameters.AddWithValue("@EmpNo", emp.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Basic", emp.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", emp.DeptNo);
+
+                cmd.ExecuteNonQuery();
+
+               
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id=0)
+        {
+            cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=J&KDec20;Integrated Security=True;Pooling=False";
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from Employees where EmpNo = " + id, cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            Employee emp = new Employee { EmpNo = (int)dr["EmpNo"], Name = (string)dr["Name"], Basic = (decimal)dr["Basic"], DeptNo = (int)dr["DeptNo"] };
+
+            cn.Close();
+            return View(emp);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Employee emp)
+        {
+            try
+            {
+                cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=J&KDec20;Integrated Security=True;Pooling=False";
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "DeleteEmployee";
+
+                cmd.Parameters.AddWithValue("@EmpNo", emp.EmpNo);
+               
+
+                cmd.ExecuteNonQuery();
+
+               
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
---
+++ Day16/FirstApplication/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "Hello <b>World</b>";
+        //}
+
+
+        //passind id 
+        public ActionResult First(int id=0)
+        { 
+            ViewBag.Id = id;
+            return View();
+        }
+
+        //passing parameters using query string 
+        //http://localhost:52202/Default/Second/1000?a=100&b=200&c="Shantanu"
+        public ActionResult Second(int id = 0,int a=0, int b = 0 , string c ="")
+        {
+            ViewBag.Id = id;
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.c = c;
+            return View();
+        }
+
+    }
+}
